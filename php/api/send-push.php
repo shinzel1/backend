3aa92ef94718955715af9c739299ffd244cf4641
sachin vinay <75784102+shinzel1@users.noreply.github.com>
2025-06-29 10:05:06 +0530

    Update send-push.php

--- php/api/send-push.php
+++ php/api/send-push.php
@@ -1,6 +1,6 @@
 <?php
-require_once realpath(__DIR__ . '/../vendor/autoload.php');
-require_once realpath(__DIR__ . '/../db.php');
+require_once __DIR__ . '/../db.php'; // ✅ Correct (goes to php/db.php)
+require_once __DIR__ . '/../vendor/autoload.php'; // ✅ Correct (goes to php/db.php)
 
 use Minishlink\WebPush\WebPush;
 use Minishlink\WebPush\Subscription;
