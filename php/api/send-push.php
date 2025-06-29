8ed04253e07a16bcf6dc26349a445f5422b9885c
sachin vinay <75784102+shinzel1@users.noreply.github.com>
2025-06-29 10:15:34 +0530

    Update send-push.php

--- php/api/send-push.php
+++ php/api/send-push.php
@@ -1,6 +1,6 @@
 <?php
-require_once './db.php';
-require_once '../vendor/autoload.php';
+require_once ('./db.php');
+require_once ('../vendor/autoload.php');
 
 use Minishlink\WebPush\WebPush;
 use Minishlink\WebPush\Subscription;
