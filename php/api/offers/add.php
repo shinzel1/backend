fa49a36467faf037c261a3bbb02947c46d097483
sachin vinay <75784102+shinzel1@users.noreply.github.com>
2025-09-10 14:33:09 +0530

    Update add.php

--- php/api/offers/add.php
+++ php/api/offers/add.php
@@ -116,11 +116,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label class="form-label">Start Date</label>
-                    <input type="date" name="start_date" class="form-control" required>
+                    <input type="date" name="start_date" class="form-control">
                 </div>
                 <div class="col-md-6 mb-3">
                     <label class="form-label">End Date</label>
-                    <input type="date" name="end_date" class="form-control" required>
+                    <input type="date" name="end_date" class="form-control">
                 </div>
             </div>
 
